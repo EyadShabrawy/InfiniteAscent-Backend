c76e18881dda41caff56ee6480c604efc5416360
Eyad Shabrawy <eyad_elshabrawy@outlook.com>
2025-04-21 19:39:21 +0200

    Adjusted port to listen to all interfaces to test on device

--- server.js
+++ server.js
@@ -117,6 +117,6 @@ app.delete('/user/:id', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://10.10.1.164:${PORT}`);
 });
